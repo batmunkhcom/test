3deb5847a3218f0bd1e2f95f023ef1ba877c657f
Batmunkh Moltov <batmunkhcom@gmail.com>
2014-08-20 13:11:19 -0800

    connect.php included...

--- includes/connect.php
+++ includes/connect.php
@@ -7,17 +7,3 @@ if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-
-$data = $mysqli->query("SELECT * FROM users ORDER BY id ASC");
-
-print_r($data);
-
-while ($obj = $data->fetch_object()) {
-    
-    echo '<br />';
-    echo $obj->id.'. ';
-    echo $obj->username.'. ';
-    echo $obj->created_at.'...';
-    
-} 
-
--- templates/home.php
+++ templates/home.php
@@ -1,1 +1,18 @@
 home page
+
+<?php
+
+
+$data = $mysqli->query("SELECT * FROM users ORDER BY id ASC");
+
+print_r($data);
+
+while ($obj = $data->fetch_object()) {
+    
+    echo '<br />';
+    echo $obj->id.'. ';
+    echo $obj->username.'. ';
+    echo $obj->created_at.'...';
+    
+} 
+
